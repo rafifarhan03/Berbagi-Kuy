1dd755b8cdb226b35c205c4cd3f8691584d0a865
Rafi Farhan <43003315+rafifarhan03@users.noreply.github.com>
2020-05-11 07:16:16 +0700

    Add files via upload
    Data

---
+++ Tubes/src/LandingPage.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class LandingPage extends JFrame{
+    private JPanel mainpanel;
+    private JTextField usernameTextField;
+    private JButton Apply;
+    private JPanel panel1;
+    private JLabel LoginLabel;
+    private JPasswordField passwordPasswordField;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
