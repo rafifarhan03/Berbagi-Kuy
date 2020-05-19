1ebfd831b21bb1f5b380a5123554dc21a1bce8bc
abrardewa <58102178+abrardewa@users.noreply.github.com>
2020-05-19 10:54:54 +0700

    Add files via upload

---
+++ Tubes/src/fix3/BodyPanel/Konfig.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fix3.BodyPanel;
+
+/**
+ *
+ * @author abrardewapratamabarus
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Konfig 
+{
+    private static Connection MySQLConfig;
+    
+    public static Connection configDB() throws SQLException{
+        try{
+            String url = "jdbc:mysql://localhost:3306/yuk_berbagi";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig = DriverManager.getConnection(url, user, pass);
+        }catch(SQLException e){
+            System.out.println("Koneksi ke Database Gagal " + e.getMessage());
+        }
+        
+        return MySQLConfig;
+    }
+    }
+
+
